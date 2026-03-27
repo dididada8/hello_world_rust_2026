fa03741a428f62d34278e170e4fd4f09e90f0582
你的用户名 <abc@abc.com>
2026-03-27 23:33:47 +0800

    test

--- src/bin/ch02_00_guessing_game_tutorial.rs
+++ src/bin/ch02_00_guessing_game_tutorial.rs
@@ -6,7 +6,8 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    guessing_game();
+    //guessing_game();
+    loop_guessing_game();
 
     println!();
 
@@ -57,4 +58,36 @@ fn guessing_game() {
     }
 
     println!("Goodbye!");
+}
+
+fn loop_guessing_game() {
+    println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
