f203aad249c23c34bdf1ea5f285227ad4546ac75
你的用户名 <abc@abc.com>
2026-03-27 21:41:40 +0800

    test

--- src/bin/ch02_00_guessing_game_tutorial.rs
+++ src/bin/ch02_00_guessing_game_tutorial.rs
@@ -8,4 +8,12 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess);
+
+
+    println!("Goodbye!");
+
+    let x = 5;
+    let y = 10;
+
+    println!("x = {x} and y + 2 = {}", y + 2);
 }
