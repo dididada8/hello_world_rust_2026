c7dc8c75311d68374e25a8ecdcf124f4e289ff90
你的用户名 <abc@abc.com>
2026-03-29 00:50:05 +0800

    test

---
+++ src/bin/ch04_02_references_and_borrowing.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+    let mut s = String::from("hello");
+
+    change(&mut s);
+    println!("The string is now: {}", s);
+}
+
+fn calculate_length(s: &String) -> usize { // s 是对 String 的引用
+    s.len()
+} // 这里，s 离开作用域。但因为 s 没有它所引用内容的所有权，
+// 所以 String 不会被丢弃。
+
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
