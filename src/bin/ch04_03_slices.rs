1f348f59a1b277d48bb5c54e563bb948c1dcf2ea
你的用户名 <abc@abc.com>
2026-03-29 15:29:55 +0800

    test

---
+++ src/bin/ch04_03_slices.rs
@@ -0,0 +1,31 @@
+fn first_word(s: &str) -> usize {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word 将得到值 5
+
+    s.clear(); // 这将清空 String，使其等于 ""
+
+    // word 仍然有值 5 在这里，但 s 不再有我们可以用值 5 有意义地使用的任何内容，
+    // 所以 word 现在完全无效了！
+    println!("The first word  is: {word}");
+
+    println!();
+
+    if s.is_empty() {
+        s.push_str("hello world");
+    }
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("The first word is: {hello}, the second word is: {world}");
+}
