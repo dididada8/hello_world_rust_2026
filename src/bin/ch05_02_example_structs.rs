7aab2a8c1ce5eb10314cb4f61bb5eaf2d231f58c
你的用户名 <abc@abc.com>
2026-03-30 01:10:03 +0800

    test

---
+++ src/bin/ch05_02_example_structs.rs
@@ -0,0 +1,33 @@
+use helloworld::print_line_separator;
+
+fn demo_1() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {rect1:?}");
+    println!("area is {}", rect1.width * rect1.height);
+}
+
+fn main() {
+    demo_1();
+    print_line_separator();
+
+    let base = 10;
+    // 1) 内部函数：不能捕获外部变量 `base`
+    fn add_fn(x: i32) -> i32 {
+        // x + base // 这里会报错：cannot find value `base` in this scope
+        x + 1
+    }
+    // 2) 闭包：可以捕获外部变量 `base`
+    let add_closure = |x: i32| x + base;
+
+    println!("add_fn(5) = {}", add_fn(5)); // 6
+    println!("add_closure(5) = {}", add_closure(5)); // 15
+}
