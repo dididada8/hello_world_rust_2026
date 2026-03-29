c7586f9f153918e4d50ab2f3b574a4b64f84e5a3
你的用户名 <abc@abc.com>
2026-03-29 15:23:20 +0800

    test

--- src/bin/ch04_02_references_and_borrowing.rs
+++ src/bin/ch04_02_references_and_borrowing.rs
@@ -7,13 +7,19 @@ fn main() {
 
     change(&mut s);
     println!("The string is now: {}", s);
+
+    let r1 = &s;
+    // let r2 = &mut s; // error: cannot borrow `s` as mutable more than once at a time
+    let r2 = &s;
+    
+    println!("{r1}, {r2}");
 }
 
-fn calculate_length(s: &String) -> usize { // s 是对 String 的引用
+fn calculate_length(s: &String) -> usize {
+    // s 是对 String 的引用
     s.len()
 } // 这里，s 离开作用域。但因为 s 没有它所引用内容的所有权，
 // 所以 String 不会被丢弃。
-
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
