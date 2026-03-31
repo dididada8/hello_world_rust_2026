012bbd973e4d8331c9076fcb7d40e9e9f1c1856b
你的用户名 <abc@abc.com>
2026-03-31 23:49:36 +0800

    test

--- crates/restaurant/src/lib.rs
+++ crates/restaurant/src/lib.rs
@@ -2,26 +2,30 @@ mod front_of_house;
 mod back_of_house;
 
 pub use crate::front_of_house::hosting;
+use crate::front_of_house::hosting::add_to_waitlist;
 
 fn deliver_order() {}
 
 pub fn eat_at_restaurant() {
-    // Absolute path
+    // 绝对路径
     crate::front_of_house::hosting::add_to_waitlist();
 
-    // Relative path
+    // 相对路径
     front_of_house::hosting::add_to_waitlist();
 
-    // Order a breakfast in the summer with Rye toast
-    let mut meal = back_of_house::breakfast::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like
+    add_to_waitlist();
+
+    // 点一份夏季早餐，黑麦吐司
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // 改变主意，想要小麦吐司
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
 
-    // The next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal fruit that comes with the meal
+    // 下面这行如果取消注释将无法编译；我们不允许
+    // 查看或修改餐食附带的时令水果
     // meal.seasonal_fruit = String::from("blueberries");
 
-    let order1 = back_of_house::appetizer::Appetizer::Soup;
-    let order2 = back_of_house::appetizer::Appetizer::Salad;
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
 }
