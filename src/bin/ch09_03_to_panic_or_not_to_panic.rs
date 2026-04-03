648c9c82eb4da7f33022a9e4ea4905eff5565d5e
你的用户名 <abc@abc.com>
2026-04-04 03:22:42 +0800

    test

--- src/bin/ch09_03_to_panic_or_not_to_panic.rs
+++ src/bin/ch09_03_to_panic_or_not_to_panic.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+use std::io;
 use std::net::IpAddr;
 
 fn demo_1() {
@@ -33,6 +35,41 @@ fn demo_3() {
     println!("home: {}", home.is_ipv4());
 }
 
+fn demo_4() {
+    println!("猜数字！");
+
+    let secret_number = rand::random_range(1..=100);
+
+    loop {
+        // --snip--
+
+        println!("请输入你的猜测。");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("读取行失败");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("秘密数字将在 1 和 100 之间。");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            // --snip--
+            Ordering::Less => println!("太小了！"),
+            Ordering::Greater => println!("太大了！"),
+            Ordering::Equal => {
+                println!("你赢了！");
+                break;
+            }
+        }
+    }
+}
 
 fn main() {
     demo_1();
@@ -40,4 +77,6 @@ fn main() {
     demo_2();
     println!();
     demo_3();
+    println!();
+    demo_4();
 }
