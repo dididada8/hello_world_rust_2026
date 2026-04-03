ebf9b7f140ca3c9deb8286ee486b432674cef0d9
你的用户名 <abc@abc.com>
2026-04-03 19:42:30 +0800

    test

--- src/bin/ch09_02_recoverable_errors_with_result.rs
+++ src/bin/ch09_02_recoverable_errors_with_result.rs
@@ -56,7 +56,7 @@ fn demo_1() {
 
 fn demo_2() {
     fn read_username_from_file() -> Result<String, io::Error> {
-        let username_file_result = File::open("hello.txt");
+        let username_file_result = File::open("hello2.txt");
 
         let mut username_file = match username_file_result {
             Ok(file) => file,
