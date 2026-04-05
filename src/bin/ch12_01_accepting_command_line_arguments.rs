2b22bc8e757e5e27a50bb93e1589d079131d3b1b
你的用户名 <abc@abc.com>
2026-04-05 18:50:45 +0800

    test

--- src/bin/ch12_01_accepting_command_line_arguments.rs
+++ src/bin/ch12_01_accepting_command_line_arguments.rs
@@ -3,6 +3,18 @@ use std::env;
 //cargo run --bin ch12_01_accepting_command_line_arguments -- searchstring example-filename.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
 
+    if args.len() < 3 {
+        eprintln!(
+            "Usage: cargo run --bin ch12_01_accepting_command_line_arguments -- <query> <file_path> \n\
+             Example: cargo run --bin ch12_01_accepting_command_line_arguments -- searchstring example-filename.txt"
+        );
+        std::process::exit(1);
+    }
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
