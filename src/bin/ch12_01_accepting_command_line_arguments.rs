5f09a78032c1909c229e70f6105d81195b844a3b
你的用户名 <abc@abc.com>
2026-04-05 19:16:51 +0800

    test

--- src/bin/ch12_01_accepting_command_line_arguments.rs
+++ src/bin/ch12_01_accepting_command_line_arguments.rs
@@ -1,7 +1,20 @@
 use std::{env, fs};
 
 //cargo run --bin ch12_01_accepting_command_line_arguments -- searchstring example-filename.txt
-fn main() {
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
+
+fn demo_1() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
@@ -18,8 +31,11 @@ fn main() {
     println!("Searching for {query}");
     println!("In file {file_path}");
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn main() {
+    demo_1();
 }
