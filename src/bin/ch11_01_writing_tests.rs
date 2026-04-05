c46113ea13502dbbdc87b1aec27ca0ae155d730f
你的用户名 <abc@abc.com>
2026-04-05 18:05:45 +0800

    test

--- src/bin/ch11_01_writing_tests.rs
+++ src/bin/ch11_01_writing_tests.rs
@@ -50,6 +50,34 @@ mod tests {
     fn test_add_zero() {
         assert_eq!(add(5, 0), 5);
     }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
 }
 
 /* ========== 在 main() 中手动运行测试 ========== */
