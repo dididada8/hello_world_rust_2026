35d30fd88d40060ce6db2126dedb9e620946653b
你的用户名 <abc@abc.com>
2026-04-08 02:40:41 +0800

    test

--- src/bin/ch15_01_box.rs
+++ src/bin/ch15_01_box.rs
@@ -1,6 +1,8 @@
 use helloworld::{print_line_separator, print_type_of};
 
 fn demo_1(){
+    #[derive(Debug)]
+    #[allow(dead_code)]
     enum List{
         Cons(i32, Box<List>),
         Nil
@@ -10,13 +12,14 @@ fn demo_1(){
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     print_type_of(&list,Some("box list demo"));
 
+
 }
 
 fn main() {
     let b = Box::new(5);
     println!("b = {b}");
     print_type_of(&b,Some("box demo"));
-    
+
     print_line_separator();
     demo_1();
 }
