f3df7cea9219ca1662fabcbceda15696e2e7c20f
你的用户名 <abc@abc.com>
2026-04-07 15:48:47 +0800

    test

--- src/bin/ch13_01_closures.rs
+++ src/bin/ch13_01_closures.rs
@@ -176,6 +176,26 @@ fn demo_5() {
     })
     .join()
     .unwrap();
+
+    println!();
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("Sorted by width: {list:?}");
+    list.sort_by_key(|r| r.height);
+    println!("Sorted by height: {list:?}");
+
 }
 
 fn main() {
