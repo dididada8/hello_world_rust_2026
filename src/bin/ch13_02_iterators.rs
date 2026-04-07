d33b57add372f117200813a5a47a1e0b2d90c8db
你的用户名 <abc@abc.com>
2026-04-07 16:34:19 +0800

    test

--- src/bin/ch13_02_iterators.rs
+++ src/bin/ch13_02_iterators.rs
@@ -9,15 +9,26 @@ fn demo_1() {
         assert_eq!(v1_iter.next(), None);
         assert_eq!(v1_iter.next(), None);
     }
+
     fn iterator_sum() {
         let v1 = vec![1, 2, 3];
         let v1_iter = v1.iter();
-        let total: i32 = v1_iter.sum();//消耗v1_iter的所有权
+        let total: i32 = v1_iter.sum(); //消耗v1_iter的所有权
         //v1_iter.next(); //错误：v1_iter 已经被 sum() 消耗掉了，不能再调用 next() 方法了
         assert_eq!(total, 6);
     }
+
+    fn iterator_change() {
+        let mut v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter_mut();
+        for item in v1_iter {
+            *item += 50;
+        }
+    }
+
     iterator_demonstration();
     iterator_sum();
+    iterator_change();
 }
 
 fn main() {
