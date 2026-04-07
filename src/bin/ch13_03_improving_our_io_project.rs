64dbb3ce1154c6598f82919fde0943e1ccdd439b
你的用户名 <abc@abc.com>
2026-04-07 17:36:40 +0800

    test

--- src/bin/ch13_03_improving_our_io_project.rs
+++ src/bin/ch13_03_improving_our_io_project.rs
@@ -1,9 +1,8 @@
-use helloworld::{search, search_case_insensitive};
-use std::{env, fs, process};
+use helloworld::{print_line_separator, search, search_case_insensitive};
 use std::error::Error;
+use std::{env, fs, process};
 
 //cargo run --bin ch13_03_improving_our_io_project -- You example-filename.txt
-
 
 fn demo_1() {
     pub struct Config {
@@ -55,7 +54,63 @@ fn demo_1() {
     }
 }
 
+fn demo_2() {
+    pub struct Config {
+        pub query: String,
+        pub file_path: String,
+        pub ignore_case: bool,
+    }
+
+    impl Config {
+        fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+            args.next();
+            let query = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a query string"),
+            };
+
+            let file_path = match args.next() {
+                Some(arg) => arg,
+                None => return Err("Didn't get a file path"),
+            };
+
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
+            Ok(Config {
+                query,
+                file_path,
+                ignore_case,
+            })
+        }
+    }
+
+    fn run(config: Config) -> Result<(), Box<dyn Error>> {
+        let contents = fs::read_to_string(config.file_path)?;
+        let results = if config.ignore_case {
+            search_case_insensitive(&config.query, &contents)
+        } else {
+            search(&config.query, &contents)
+        };
+        for line in results {
+            println!("{line}");
+        }
+        Ok(())
+    }
+
+    let args = env::args();
+    let config = Config::build(args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
 fn main() {
     demo_1();
-
+    println!();
+    print_line_separator();
+    demo_2();
 }
