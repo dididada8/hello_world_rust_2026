e3b20400996a9e66978be53306deb487d176db49
你的用户名 <abc@abc.com>
2026-04-12 01:45:46 +0800

    test

--- src/bin/ch17_02_concurrency_with_async.rs
+++ src/bin/ch17_02_concurrency_with_async.rs
@@ -1,6 +1,14 @@
 use helloworld::{print_line_separator, print_type_of};
+use std::thread;
 
 fn demo_1() {
+    let channel = thread::spawn(|| {
+        for i in 1..=5 {
+            println!("__hi number {i} from the channel thread!");
+            std::thread::sleep(std::time::Duration::from_millis(500));
+        }
+    });
+
     trpl::block_on(async {
         let handle = trpl::spawn_task(async {
             for i in 1..=5 {
@@ -15,6 +23,9 @@ fn demo_1() {
         }
         print_type_of(&handle, Some("demo_1:JoinHandle<()>"));
     });
+
+
+    channel.join().unwrap();
 }
 
 fn main() {
